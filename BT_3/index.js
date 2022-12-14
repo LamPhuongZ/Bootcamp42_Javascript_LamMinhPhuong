4e48caa17e0bb43452ba88310d51a62b6cf55f69
LamPhuongZ <lamphuong024@gmail.com>
2022-12-14 18:58:48 +0700

    add file BT_3, add index.html, add index.js

---
+++ BT_3/index.js
@@ -0,0 +1,13 @@
+// Bài tập 3: Quy đổi tiền
+// input: nhập vào số tiền USD
+let USDprice = 23500;
+let USD = 2;
+// out put: tính và xuất ra số tiền sau quy đổi VND
+let VNDprice = 0;
+
+// progress
+VNDprice = USD * USDprice;
+
+console.log("Số tiền quy đổi VND: ", VNDprice);
+
+
