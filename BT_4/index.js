27a657a8b2461742431ab19349dcd07863735b11
LamPhuongZ <lamphuong024@gmail.com>
2022-12-14 19:14:14 +0700

    add BT_3, add index.html, add index.js

---
+++ BT_4/index.js
@@ -0,0 +1,16 @@
+// Bài tập 4: Tính diện tích, chu vi hình chữ nhật
+// input: nhập vào 2 chiều dài và chiều rộng của HCN
+let CHIEU_DAI = 10;
+let CHIEU_RONG = 5;
+// out put: tính và xuất diện tích, chu vi của HCN
+let DIEN_TICH = 0;
+let CHU_VI = 0;
+
+// progress
+DIEN_TICH = CHIEU_DAI * CHIEU_RONG;
+CHU_VI = (CHIEU_DAI + CHIEU_RONG) * 2;
+
+console.log("Diện tích hình chữ nhật: ", DIEN_TICH);
+console.log("Chu vi hình chữ nhật: ", CHU_VI);
+
+
