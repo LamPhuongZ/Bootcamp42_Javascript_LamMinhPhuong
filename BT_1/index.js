e413a2a9b087cf3df3de2bac033e3b347d4c3a8d
LamPhuongZ <lamphuong024@gmail.com>
2022-12-14 18:31:33 +0700

    add file BT_1, add index.html, add index.js

---
+++ BT_1/index.js
@@ -0,0 +1,10 @@
+// Bài tập 1: Tính tiền lương nhân viên
+// input: nhập vào số ngày làm việc
+let salaryOneday = 100000;
+let number = 26;
+// out put: tính tiền lương nhân viên
+let salary =  0;
+// progress
+salary = salaryOneday * number;
+
+console.log("Tiền lương nhân viên: ", salary);
